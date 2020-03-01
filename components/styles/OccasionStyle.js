9b114744da88f9644a8edc0a61ae00e9eb0b24f0
ErnieBuyco <buycoernie@gmail.com>
2020-03-01 13:33:38 +0800

    occasion__update

--- components/styles/OccasionStyle.js
+++ components/styles/OccasionStyle.js
@@ -10,17 +10,18 @@ const OccasionStyle = styled.section`
   .grid__boxes__wrapper {
     display: flex;
     flex-flow: row wrap;
-    justify-content: flex-start;
+    /* justify-content: flex-start; */
+    justify-content: center;
+    align-items: center;
   }
   .grid__items {
     height: auto;
     flex-basis: 30%;
     flex: auto;
     width: 100%;
-    padding: 10px;
     box-sizing: border-box;
     max-width: 20%;
-    background-color: red;
+    background-color: ${props => props.theme.colors.gray.g100};
     margin: 1rem;
     @media (max-width: 1333px) {
       flex-basis: 33.33%;
@@ -34,6 +35,28 @@ const OccasionStyle = styled.section`
     @media (max-width: 555px) {
       flex-basis: 100%;
     }
+  }
+  .img__banner__birthday {
+    width: 100%;
+    height: auto;
+  }
+  .text__caption__font {
+    display: flex;
+    justify-content: flex-start;
+    font-family: 'Nunitolight';
+  }
+  .wrapper__content__inner {
+    padding: 2rem;
+  }
+  .btn__occasion__seemore {
+    display: flex;
+    cursor: pointer;
+    margin-top: 4rem;
+  }
+  .image__icon {
+    width: 100%;
+    max-width: 2%;
+    margin-left: 0.5555rem;
   }
 `;
 
--- components/subcomponent/Occasion.js
+++ components/subcomponent/Occasion.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import OccasionStyle from '../styles/OccasionStyle';
+import Birthday from '../../public/static/img/Media.png';
+import Anniversary from '../../public/static/img/Media1.png';
+import Valentines from '../../public/static/img/Media2.png';
+import Christmas from '../../public/static/img/Media3.png';
+import Arrow from '../../public/static/img/arrow--left.svg';
 
 const Occupation = () => (
   <>
@@ -12,10 +17,86 @@ const Occupation = () => (
       </div>
       <div className="container__boxes">
         <div className="grid__boxes__wrapper">
-          <div className="grid__items">1</div>
-          <div className="grid__items">2</div>
-          <div className="grid__items">3</div>
-          <div className="grid__items">4</div>
+          <div className="grid__items">
+            <div className="card__image">
+              <img
+                src={Birthday}
+                className="img__banner__birthday"
+                alt="birthday"
+              />
+            </div>
+            <div className="wrapper__content__inner">
+              <p className="text__caption__font">Birthday</p>
+              <a className="btn__occasion__seemore">
+                See More{' '}
+                <img
+                  className="image__icon"
+                  src={Arrow}
+                  alt="image__icon__name"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="grid__items">
+            <div className="card__image">
+              <img
+                src={Anniversary}
+                className="img__banner__birthday"
+                alt="birthday"
+              />
+            </div>
+            <div className="wrapper__content__inner">
+              <p className="text__caption__font">Anniversary</p>
+              <a className="btn__occasion__seemore">
+                See More{' '}
+                <img
+                  className="image__icon"
+                  src={Arrow}
+                  alt="image__icon__name"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="grid__items">
+            <div className="card__image">
+              <img
+                src={Valentines}
+                className="img__banner__birthday"
+                alt="birthday"
+              />
+            </div>
+            <div className="wrapper__content__inner">
+              <p className="text__caption__font">Valentine's Day</p>
+              <a className="btn__occasion__seemore">
+                See More{' '}
+                <img
+                  className="image__icon"
+                  src={Arrow}
+                  alt="image__icon__name"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="grid__items">
+            <div className="card__image">
+              <img
+                src={Christmas}
+                className="img__banner__birthday"
+                alt="birthday"
+              />
+            </div>
+            <div className="wrapper__content__inner">
+              <p className="text__caption__font">Christmas</p>
+              <a className="btn__occasion__seemore">
+                See More{' '}
+                <img
+                  className="image__icon"
+                  src={Arrow}
+                  alt="image__icon__name"
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </OccasionStyle>
