6ed83da0316793dfceef5ec502f6f3dbaa986dd1
ErnieBuyco <buycoernie@gmail.com>
2020-03-02 22:14:21 +0800

    respopnsive__banner__issue

--- components/styles/PromoBanner.js
+++ components/styles/PromoBanner.js
@@ -22,6 +22,11 @@ const PromoBannerStyle = styled.section`
     flex: auto;
     width: 100%;
     max-width: 50%;
+    @media (max-width: ${props => props.theme.medium}) {
+      flex-basis: 100%;
+      max-width: 100%;
+      margin-bottom: 2rem;
+    }
   }
   .promo__banner__button__wrapper {
     flex-basis: 50%;
@@ -29,12 +34,30 @@ const PromoBannerStyle = styled.section`
     flex: auto;
     width: 100%;
     max-width: 50%;
+    @media (max-width: ${props => props.theme.medium}) {
+      flex-basis: 100%;
+      max-width: 100%;
+      text-align: center;
+      margin: 0 auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-flow: column;
+    }
   }
   .promo__header {
     font-family: 'NunitoSemiBold';
+    font-size: 2.5vw;
+    @media (max-width: ${props => props.theme.small}) {
+      font-size: 4.5vw;
+    }
   }
   .promo__sub__context {
     font-family: 'Nunitolight';
+    font-size: 2vw;
+    @media (max-width: ${props => props.theme.small}) {
+      font-size: 3.5vw;
+    }
   }
   .promo__button__fill {
     color: #fff;
@@ -43,6 +66,12 @@ const PromoBannerStyle = styled.section`
     border-radius: 0.3rem;
     border: 0 solid ${props => props.theme.colors.gray.g1200};
     text-transform: uppercase;
+    @media (max-width: ${props => props.theme.small}) {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
   .promo__button__outline {
     color: #fff;
@@ -52,6 +81,13 @@ const PromoBannerStyle = styled.section`
     border: 1px solid ${props => props.theme.colors.gray.g1200};
     text-transform: uppercase;
     margin-left: 1vw;
+    @media (max-width: ${props => props.theme.small}) {
+      width: 100%;
+      margin-top: 2vw;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
 `;
 
