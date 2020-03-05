c6edd1e5b22c0fd2b8fb130dafe52e25bf5697dd
ErnieBuyco <buycoernie@gmail.com>
2020-03-05 12:55:08 +0800

    test_deploy

--- components/styles/FooterStyle.js
+++ components/styles/FooterStyle.js
@@ -5,6 +5,33 @@ const FooterStyle = styled.section`
   border: 0 solid ${props => props.theme.colors.gray.g1000};
   color: ${props => props.theme.colors.g200};
   font-family: 'NunitoRegular';
+  .footer__navigation__button {
+    width: 100%;
+    max-width: 70%;
+    margin: 0 auto;
+  }
+  .wrapper__navigation {
+    list-style: none;
+    margin: 0 auto;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .navigation__list {
+    flex: 1 5 20px;
+  }
+  .logo_bottom__img {
+    width: 20%;
+    height: auto;
+    margin: 0 auto;
+  }
+  .navigation__list__logo {
+    margin: 0 auto;
+    justify-content: center;
+    align-items: center;
+    flex: 1 1 20px;
+  }
 `;
 
 export default FooterStyle;
