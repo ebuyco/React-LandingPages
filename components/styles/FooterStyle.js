04a72dcad790811d719c1da42e7950c73ae1213c
ErnieBuyco <buycoernie@gmail.com>
2020-03-05 15:51:42 +0800

    footer__update

--- components/styles/FooterStyle.js
+++ components/styles/FooterStyle.js
@@ -2,13 +2,24 @@ import styled from 'styled-components';
 
 const FooterStyle = styled.section`
   width: 100%;
-  border: 0 solid ${props => props.theme.colors.gray.g1000};
+  background-color: ${props => props.theme.colors.gray.g1000};
   color: ${props => props.theme.colors.g200};
   font-family: 'NunitoRegular';
   .footer__navigation__button {
     width: 100%;
     max-width: 70%;
     margin: 0 auto;
+
+    &:after {
+      height: 2px;
+      content: '';
+      width: 100%;
+      max-width: 70%;
+      position: absolute;
+      left: 15vw;
+      margin-top: 2vw;
+      border-top: 1px solid ${props => props.theme.colors.gray.g1200};
+    }
   }
   .wrapper__navigation {
     list-style: none;
@@ -20,6 +31,7 @@ const FooterStyle = styled.section`
   }
   .navigation__list {
     flex: 1 5 20px;
+    font-family: 'NunitoSemiBold';
   }
   .logo_bottom__img {
     width: 20%;
@@ -31,6 +43,32 @@ const FooterStyle = styled.section`
     justify-content: center;
     align-items: center;
     flex: 1 1 20px;
+  }
+  .footer__inner {
+    padding: 4vw;
+  }
+  .social__wrapper__icons {
+    width: 100%;
+    height: auto;
+    margin: 0 auto;
+  }
+  .social__inner__icons {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    margin-bottom: 4vw;
+    margin-top: 2vw;
+  }
+  .icons__content {
+    flex: 0 0 5vw;
+  }
+  .copyright__inner {
+    width: 100%;
+    margin: 0 auto;
+    font-family: 'NunitoRegular';
+  }
+  .copyright--title {
+    font-family: 'Nunitolight';
   }
 `;
 
--- components/subcomponent/Footer.js
+++ components/subcomponent/Footer.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import FooterStyle from '../styles/FooterStyle';
 import LogoFooter from '../../public/static/img/logo.png';
+import SocialFacebook from '../../public/static/img/social-facebook.svg';
+import SocialTwitter from '../../public/static/img/social-twitter.svg';
+import SocialInstagram from '../../public/static/img/social-instagram.svg';
+import SocialYoutube from '../../public/static/img/social-youtube.svg';
+import SocialPinterest from '../../public/static/img/social-pinterest.svg';
 
 const Footer = () => (
   <>
@@ -22,6 +27,48 @@ const Footer = () => (
               <li className="navigation__list">Blog</li>
             </ul>
           </div>
+        </div>
+        <div className="social__wrapper__icons">
+          <div className="social__inner__icons">
+            <a href="/" className="icons__content">
+              <img
+                src={SocialFacebook}
+                className="icon__content__inner"
+                alt="name__icon"
+              />
+            </a>
+            <a href="/" className="icons__content">
+              <img
+                src={SocialTwitter}
+                className="icon__content__inner"
+                alt="name__icon"
+              />
+            </a>
+            <a href="/" className="icons__content">
+              <img
+                src={SocialInstagram}
+                className="icon__content__inner"
+                alt="name__icon"
+              />
+            </a>
+            <a href="/" className="icons__content">
+              <img
+                src={SocialYoutube}
+                className="icon__content__inner"
+                alt="name__icon"
+              />
+            </a>
+            <a href="/" className="icons__content">
+              <img
+                src={SocialPinterest}
+                className="icon__content__inner"
+                alt="name__icon"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="copyright__inner">
+          <h4 className="copyright--title">© 2010-2020 Privacy-Terms</h4>
         </div>
       </div>
     </FooterStyle>
