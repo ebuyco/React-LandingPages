5a8c149fbde867d1df1d679c9e18f60ac60d2780
ErnieBuyco <buycoernie@gmail.com>
2020-03-05 12:43:12 +0800

    logo__added

--- components/subcomponent/Footer.js
+++ components/subcomponent/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import FooterStyle from '../styles/FooterStyle';
+import LogoFooter from '../../public/static/img/logo.png';
 
 const Footer = () => (
   <>
     <FooterStyle>
-      <h1>Test</h1>
+      <div className="footer__wrapper">
+        <div className="footer__inner">
+          <div className="footer__navigation__button">
+            <ul className="wrapper__navigation">
+              <li className="navigation__list">Home</li>
+              <li className="navigation__list">About</li>
+              <li className="navigation__list__logo">
+                <img
+                  className="logo_bottom__img"
+                  src={LogoFooter}
+                  alt="name__logo"
+                />
+              </li>
+              <li className="navigation__list">Service</li>
+              <li className="navigation__list">Blog</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </FooterStyle>
   </>
 );
